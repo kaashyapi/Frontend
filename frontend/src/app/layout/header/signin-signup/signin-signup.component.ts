aab4fbfb4c93a17248c2a9968c678752c914b52b
Kashyapi Mistry <kashyapikm@gmail.com>
2023-07-04 14:03:34 +0530

    Updated

--- frontend/src/app/layout/header/signin-signup/signin-signup.component.ts
+++ frontend/src/app/layout/header/signin-signup/signin-signup.component.ts
@@ -85,7 +85,7 @@ export class SigninSignupComponent implements OnInit {
             'userIdExpiration',
             expirationTime.toISOString()
           );
-          const user = JSON.stringify(response.user)
+          const user = JSON.stringify(response.data.user)
           localStorage.setItem('name', user);
           this.snackBar.open(response.message, 'Dismiss', commonSnackBarConfig);
           this.dialogRef.close();

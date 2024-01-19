28710ad6728cf3c29ab7e87a7e88aab6ba57ca89
kaashyapi <kashyapikm@gmail.com>
2024-01-19 13:44:22 +0530

    Updated signin-signup

--- frontend/src/app/layout/header/signin-signup/signin-signup.component.ts
+++ frontend/src/app/layout/header/signin-signup/signin-signup.component.ts
@@ -1,3 +1,180 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ForgotpasswordComponent } from '../forgotpassword/forgotpassword.component';
+// import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+// import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+// import { AuthService } from 'src/app/service/auth.service';
+// import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+// import { commonSnackBarConfig } from 'src/app/service/snackbar-config.service';
+// import { MatSnackBar } from '@angular/material/snack-bar';
+// import { NgxUiLoaderService } from 'ngx-ui-loader';
+// @Component({
+//   selector: 'app-signin-signup',
+//   templateUrl: './signin-signup.component.html',
+//   styleUrls: ['./signin-signup.component.css'],
+// })
+// export class SigninSignupComponent implements OnInit {
+//   isMobile!: boolean;
+//   selectedTabIndex = 0;
+//   signInForm!: FormGroup;
+//   signUpForm!: FormGroup;
+//   userRole: string;
+//   constructor(
+//     private breakpointObserver: BreakpointObserver,
+//     private fb: FormBuilder,
+//     private authService: AuthService,
+//     public dialogRef: MatDialogRef<SigninSignupComponent>,
+//     public dialog: MatDialog,
+//     private snackBar: MatSnackBar,
+//     private ngxLoader: NgxUiLoaderService
+//   ) {
+//     this.breakpointObserver.observe(Breakpoints.Handset).subscribe((result) => {
+//       this.isMobile = result.matches;
+//     });
+//   }
+//   openSignInDialog(): void {
+//     this.dialog.open(SigninSignupComponent, {
+//       width: 'auto',
+//     });
+//   }
+//   openForgotPasswordDialog(): void {
+//     this.dialog.open(ForgotpasswordComponent, {
+//       width: 'auto',
+//     });
+//   }
+//   public userId: any = localStorage.getItem('userId');
+
+//   ngOnInit(): void {
+//     this.createSignInForm();
+//     this.createSignUpForm();
+//   }
+
+//   createSignInForm(): void {
+//     this.signInForm = this.fb.group({
+//       email: ['', [Validators.required, Validators.email]],
+//       password: ['', [Validators.required, Validators.minLength(8)]],
+//     });
+//   }
+
+//   createSignUpForm(): void {
+//     const regex = new RegExp(
+//       "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
+//     );
+//     this.signUpForm = this.fb.group({
+//       firstName: [
+//         '',
+//         [Validators.required, Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*")],
+//       ],
+//       lastName: [
+//         '',
+//         [Validators.required, Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*")],
+//       ],
+//       email: ['', [Validators.required, Validators.pattern(regex)]],
+//     });
+//   }
+
+//   onSignIn(): void {
+//     if (this.signInForm.valid) {
+//       this.ngxLoader.start();
+//       this.authService.signIn(this.signInForm.value).subscribe(
+//         (response) => {
+//           this.ngxLoader.stop();
+//           localStorage.setItem('name', response.user.name);
+//           localStorage.setItem('userId', response.user.userId);
+//           const expirationTime = new Date(Date.now() + 12 * 60 * 60 * 1000);
+//           localStorage.setItem(
+//             'userIdExpiration',
+//             expirationTime.toISOString()
+//           );
+//           this.snackBar.open(response.message, 'Dismiss', commonSnackBarConfig);
+//           this.dialogRef.close();
+//           this.authService.isSignedIn = true;
+//           this.authService.authChanged.emit(true);
+//         },
+//         (error) => {
+//           this.ngxLoader.stop();
+//           this.snackBar.open(
+//             error.error.message,
+//             'Dismiss',
+//             commonSnackBarConfig
+//           );
+//         }
+//       );
+//     } else {
+//       return this.signInForm.markAllAsTouched();
+//     }
+//   }
+
+//   onSignUp(): void {
+//     if (this.signUpForm.valid) {
+//       this.ngxLoader.start();
+//       this.authService.signUp(this.signUpForm.value).subscribe(
+//         (response) => {
+//           this.ngxLoader.stop();
+//           this.snackBar.open(
+//             response.message + ' Please Sign In to continue',
+//             'Dismiss',
+//             commonSnackBarConfig
+//           );
+//           this.selectedTabIndex = 0;
+//         },
+//         (error) => {
+//           this.ngxLoader.stop();
+//           this.snackBar.open(
+//             error.error.message,
+//             'Dismiss',
+//             commonSnackBarConfig
+//           );
+//         }
+//       );
+//     } else {
+//       return this.signUpForm.markAllAsTouched();
+//     }
+//   }
+
+//   onForgotPassword() {
+//     this.dialogRef.close();
+//     this.openForgotPasswordDialog();
+//   }
+//   getErrorMessage(controlName: string) {
+//     const control = this.signInForm.get(controlName);
+//     if (control?.hasError('required')) {
+//       return 'This field is required';
+//     }
+//     if (controlName === 'emailId' && control?.hasError('email')) {
+//       return 'Please enter valid EmailId';
+//     }
+//     if (controlName === 'password' && control?.hasError('minlength')) {
+//       return 'Minimum length 6';
+//     }
+
+//     return '';
+//   }
+//   getErrorMessageS(controlName: string) {
+//     const control = this.signUpForm.get(controlName);
+//     if (control?.hasError('required')) {
+//       return 'This field is required';
+//     }
+//     if (controlName === 'firstName' && control?.hasError('pattern')) {
+//       return 'Ex:Tom';
+//     }
+//     if (controlName === 'lastName' && control?.hasError('pattern')) {
+//       return 'Ex:Holland';
+//     }
+//     if (controlName === 'emailId' && control?.hasError('pattern')) {
+//       return 'Enter valid EmailId';
+//     }
+//     if (controlName === 'password' && control?.hasError('minlength')) {
+//       return 'Minimum length 6';
+//     }
+//     if (controlName === 'password' && control?.hasError('pattern')) {
+//       return '1 Number,specialChar & Capital Letter';
+//     }
+
+//     return '';
+//   }
+// }
+
+
 import { Component, OnInit } from '@angular/core';
 import { ForgotpasswordComponent } from '../forgotpassword/forgotpassword.component';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
@@ -6,7 +183,7 @@ import { AuthService } from 'src/app/service/auth.service';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { commonSnackBarConfig } from 'src/app/service/snackbar-config.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { NgxUiLoaderService } from 'ngx-ui-loader';
+
 @Component({
   selector: 'app-signin-signup',
   templateUrl: './signin-signup.component.html',
@@ -24,20 +201,19 @@ export class SigninSignupComponent implements OnInit {
     private authService: AuthService,
     public dialogRef: MatDialogRef<SigninSignupComponent>,
     public dialog: MatDialog,
-    private snackBar: MatSnackBar,
-    private ngxLoader: NgxUiLoaderService
+    private snackBar: MatSnackBar
   ) {
     this.breakpointObserver.observe(Breakpoints.Handset).subscribe((result) => {
       this.isMobile = result.matches;
     });
   }
   openSignInDialog(): void {
-    this.dialog.open(SigninSignupComponent, {
+    const dialogRef = this.dialog.open(SigninSignupComponent, {
       width: 'auto',
     });
   }
   openForgotPasswordDialog(): void {
-    this.dialog.open(ForgotpasswordComponent, {
+    const dialogRef = this.dialog.open(ForgotpasswordComponent, {
       width: 'auto',
     });
   }
@@ -50,8 +226,8 @@ export class SigninSignupComponent implements OnInit {
 
   createSignInForm(): void {
     this.signInForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(8)]],
+      emailId: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
@@ -59,39 +235,66 @@ export class SigninSignupComponent implements OnInit {
     const regex = new RegExp(
       "([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|\"([]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(.[!#-'*+/-9=?A-Z^-~-]+)*|[[\t -Z^-~]*])"
     );
-    this.signUpForm = this.fb.group({
-      firstName: [
-        '',
-        [Validators.required, Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*")],
-      ],
-      lastName: [
-        '',
-        [Validators.required, Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*")],
-      ],
-      email: ['', [Validators.required, Validators.pattern(regex)]],
-    });
+    const pswd = new RegExp(
+      '^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$'
+    );
+    this.signUpForm = this.fb.group(
+      {
+        firstName: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*"),
+          ],
+        ],
+        lastName: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern("([A-Z][a-z]*)([\\s\\'-])*"),
+          ],
+        ],
+        emailId: ['', [Validators.required, Validators.pattern(regex)]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(pswd),
+            Validators.minLength(6),
+          ],
+        ],
+        confirmPassword: ['', [Validators.required]],
+      },
+      { validator: this.checkPasswords }
+    );
   }
-
+  checkPasswords(group: FormGroup) {
+    const password = group.get('password');
+    const confirmPassword = group.get('confirmPassword');
+    return password &&
+      confirmPassword &&
+      password.value === confirmPassword.value
+      ? null
+      : { passwordMismatch: true };
+  }
   onSignIn(): void {
     if (this.signInForm.valid) {
-      this.ngxLoader.start();
       this.authService.signIn(this.signInForm.value).subscribe(
         (response) => {
-          this.ngxLoader.stop();
-          localStorage.setItem('name', response.user.name);
-          localStorage.setItem('userId', response.user.userId);
           const expirationTime = new Date(Date.now() + 12 * 60 * 60 * 1000);
+          localStorage.setItem('userId', response.data._id);
           localStorage.setItem(
             'userIdExpiration',
             expirationTime.toISOString()
           );
+          localStorage.setItem('name', response.data.name);
+          console.log(response);
           this.snackBar.open(response.message, 'Dismiss', commonSnackBarConfig);
           this.dialogRef.close();
           this.authService.isSignedIn = true;
           this.authService.authChanged.emit(true);
         },
         (error) => {
-          this.ngxLoader.stop();
           this.snackBar.open(
             error.error.message,
             'Dismiss',
@@ -106,10 +309,9 @@ export class SigninSignupComponent implements OnInit {
 
   onSignUp(): void {
     if (this.signUpForm.valid) {
-      this.ngxLoader.start();
       this.authService.signUp(this.signUpForm.value).subscribe(
         (response) => {
-          this.ngxLoader.stop();
+          console.log(response);
           this.snackBar.open(
             response.message + ' Please Sign In to continue',
             'Dismiss',
@@ -118,7 +320,6 @@ export class SigninSignupComponent implements OnInit {
           this.selectedTabIndex = 0;
         },
         (error) => {
-          this.ngxLoader.stop();
           this.snackBar.open(
             error.error.message,
             'Dismiss',
@@ -127,7 +328,11 @@ export class SigninSignupComponent implements OnInit {
         }
       );
     } else {
-      return this.signUpForm.markAllAsTouched();
+      this.snackBar.open(
+        'Password not matched',
+        'Dismiss',
+        commonSnackBarConfig
+      );
     }
   }
 
--- frontend/src/app/service/auth.service.ts
+++ frontend/src/app/service/auth.service.ts
@@ -1,6 +1,77 @@
+// import { HttpClient } from '@angular/common/http';
+// import { EventEmitter, Injectable } from '@angular/core';
+// import { BehaviorSubject, Observable } from 'rxjs';
+// import { SigninSignupComponent } from '../layout/header/signin-signup/signin-signup.component';
+// import { MatDialog } from '@angular/material/dialog';
+// import { environment } from 'src/environments/environment.prod';
+// @Injectable({
+//   providedIn: 'root',
+// })
+// export class AuthService {
+//   private baseUrl = environment.baseUrl;
+//   public isSignedIn: boolean = false;
+//   authChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
+//   constructor(private http: HttpClient, private dialog: MatDialog) {}
+
+//   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
+
+//   isUserAuthenticated(): boolean {
+//     const userId = localStorage.getItem('userId');
+//     const expirationTime = localStorage.getItem('userIdExpiration');
+//     if (!userId || !expirationTime || new Date(expirationTime) < new Date()) {
+//       localStorage.removeItem('userId');
+//       localStorage.removeItem('userIdExpiration');
+//       return false;
+//     } else {
+//       this.isAuthenticatedSubject.next(true);
+//       return true;
+//     }
+//   }
+//   getIsAuthenticated(): Observable<boolean> {
+//     return this.isAuthenticatedSubject.asObservable();
+//   }
+//   openSignInDialog(): void {
+//     this.dialog.open(SigninSignupComponent, {
+//       width: 'auto',
+//     });
+//   }
+//   signIn(payload: { email: string; password: string }): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/users/signin`, payload, {
+//       withCredentials: true,
+//     });
+//   }
+
+//   signUp(body: {
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//   }): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/signup`, body);
+//   }
+
+//   forgotPassword(body: { email: string }): Observable<any> {
+//     return this.http.post(`${this.baseUrl}/users/forgotpassword`, body);
+//   }
+//   resetPassword(data: any): Observable<any> {
+//     return this.http.post(
+//       `${this.baseUrl}/forgotpassword/reset-password`,
+//       data
+//     );
+//   }
+//   signOut(data: any): Observable<any> {
+//     localStorage.removeItem('userId');
+//     localStorage.removeItem('userIdExpiration');
+//     localStorage.removeItem('name');
+//     return this.http.post(`${this.baseUrl}/users/signout`, data);
+//   }
+// }
+
+
 import { HttpClient } from '@angular/common/http';
 import { EventEmitter, Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of } from 'rxjs';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { commonSnackBarConfig } from './snackbar-config.service';
 import { SigninSignupComponent } from '../layout/header/signin-signup/signin-signup.component';
 import { MatDialog } from '@angular/material/dialog';
 import { environment } from 'src/environments/environment.prod';
@@ -11,7 +82,11 @@ export class AuthService {
   private baseUrl = environment.baseUrl;
   public isSignedIn: boolean = false;
   authChanged: EventEmitter<boolean> = new EventEmitter<boolean>();
-  constructor(private http: HttpClient, private dialog: MatDialog) {}
+  constructor(
+    private http: HttpClient,
+    private snackBar: MatSnackBar,
+    private dialog: MatDialog
+  ) {}
 
   private isAuthenticatedSubject = new BehaviorSubject<boolean>(false);
 
@@ -31,7 +106,7 @@ export class AuthService {
     return this.isAuthenticatedSubject.asObservable();
   }
   openSignInDialog(): void {
-    this.dialog.open(SigninSignupComponent, {
+    const dialogRef = this.dialog.open(SigninSignupComponent, {
       width: 'auto',
     });
   }
@@ -41,20 +116,21 @@ export class AuthService {
     });
   }
 
-  signUp(body: {
+  signUp(user: {
     firstName: string;
     lastName: string;
     email: string;
+    password: string;
   }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/signup`, body);
+    return this.http.post(`${this.baseUrl}/signup`, user);
   }
 
-  forgotPassword(body: { email: string }): Observable<any> {
-    return this.http.post(`${this.baseUrl}/users/forgotpassword`, body);
+  forgotPassword(emailId: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/forgotpassword`, emailId);
   }
   resetPassword(data: any): Observable<any> {
     return this.http.post(
-      `${this.baseUrl}/forgotpassword/reset-password`,
+      `${this.baseUrl}/forgotpassword/reset-password/`,
       data
     );
   }
